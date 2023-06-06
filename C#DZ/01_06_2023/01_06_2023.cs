1f750a14cd5f20db9d9de78cdc62e661ed38191d
fflowerpot <126529452+fflowerpot@users.noreply.github.com>
2023-06-07 02:27:35 +0300

    01_06_2023

---
+++ C#DZ/01_06_2023/01_06_2023.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_DZ._01_06_2023
+{
+    //class Item
+    //{
+    //    public string Name { get; }
+    //    public double Price { get; }
+
+    //    public Item(string name, double price)
+    //    {
+    //        Name = name;
+    //        Price = price;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return $"{Name} - {Price}";
+    //    }
+    //}
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        List<Item> availableItems = new List<Item>()
+    //    {
+    //        new Item("Телефон", 1000),
+    //        new Item("Ноутбук", 2000),
+    //        new Item("Телевізор", 1500),
+    //        new Item("Годинник", 500),
+    //        new Item("Книга", 20)
+    //    };
+
+    //        Dictionary<Item, int> cart = new Dictionary<Item, int>();
+
+    //        Console.WriteLine("Доступні товари:");
+    //        for (int i = 0; i < availableItems.Count; i++)
+    //        {
+    //            Console.WriteLine($"{i + 1}." + availableItems[i]);
+    //        }
+
+    //        while (true)
+    //        {
+    //            Console.Write("Виберіть товар (або -1 для завершення покупок): ");
+    //            int choice = int.Parse(Console.ReadLine());
+
+    //            if (choice == -1)
+    //                break;
+
+    //            if (choice < 1 || choice > availableItems.Count)
+    //            {
+    //                Console.WriteLine("Невірний вибір товару. Спробуйте ще раз.");
+    //                continue;
+    //            }
+
+    //            Item selected = availableItems[choice - 1];
+
+    //            Console.Write("Введіть кількість товару: ");
+    //            int quantity = int.Parse(Console.ReadLine());
+
+    //            if (quantity < 1)
+    //            {
+    //                Console.WriteLine("Некоректна кількість товару. Спробуйте ще раз.");
+    //                continue;
+    //            }
+
+    //            if (cart.ContainsKey(selected))
+    //            {
+    //                int currentQuantity = cart[selected];
+    //                cart[selected] = currentQuantity + quantity;
+    //            }
+    //            else
+    //            {
+    //                cart[selected] = quantity;
+    //            }
+    //        }
+
+    //        Console.Clear();
+    //        Console.WriteLine("Ваш кошик:");
+
+    //        double totalCost = 0;
+    //        foreach (var entry in cart)
+    //        {
+    //            Item item = entry.Key;
+    //            int quantity = entry.Value;
+    //            double itemCost = item.Price * quantity;
+    //            Console.WriteLine($"{item.Name} - {item.Price} x {quantity} = {itemCost}");
+    //            totalCost += itemCost;
+    //        }
+
+    //        Console.WriteLine($"Загальна вартість: {totalCost}");
+    //    }
+    //}
+
+
+}
---
+++ C#DZ/02_06_2023/02_06_2023.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_DZ._02_06_2023
+{
+    //клас для черги з пріорітетом
+
+    public class PriorityQueue<T>
+    {
+        private Dictionary<T, int> items;
+        private List<T> queue;
+
+        public int Count { get { return queue.Count; } }
+
+        public PriorityQueue()
+        {
+            items = new Dictionary<T, int>();
+            queue = new List<T>();
+        }
+
+        public void Enqueue(T item, int priority)
+        {
+            items[item] = priority;
+            int index = 0;
+            while (index < queue.Count && items[queue[index]] <= priority)//підглянув код в мережі якщо я правильно розумію в items[queue[index]] <= priority - береться елемент з ліста за відповідним ітераторо який підставляється в Dictionary і за допомогою нього викликається його пріоритет який вже й порівнюється з новим пріортетом
+            {
+                index++;
+            }
+            queue.Insert(index, item);
+        }
+
+        public T Dequeue()
+        {
+            if (queue.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            T item = queue[0];
+            queue.RemoveAt(0);
+            items.Remove(item);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (queue.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return queue[0];
+        }
+    }
+
+    class Program
+    {
+        //--1--//  -не певний що можна цей метод тут ставити але, так як він елементарний просто хотів показати що зробив
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            //--2--//
+            PriorityQueue<string> queue = new PriorityQueue<string>();
+
+            // Додавання елементів до черги з вказанням пріоритету
+            queue.Enqueue("Item 1", 2);
+            queue.Enqueue("Item 2", 1);
+            queue.Enqueue("Item 3", 3);
+
+            // Вилучення елементів з черги
+            while (queue.Count > 0)
+            {
+                string item = queue.Dequeue();
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+
+}
