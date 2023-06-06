971f9756fbf8bc054a881ea2bb9b5a2aaee46c36
fflowerpot <126529452+fflowerpot@users.noreply.github.com>
2023-06-06 20:15:52 +0300

    26_05_2023

--- C#DZ/25_05_2023/25_05_2023.cs
+++ C#DZ/25_05_2023/25_05_2023.cs
@@ -6,137 +6,137 @@ using System.Threading.Tasks;
 
 namespace C_DZ._25_05_2023
 {
-    // фібоначчі
-    public static class IntExtensions
-    {
-        public static bool IsFibonacciNumber(this int number)
-        {
-            if (number < 0)
-                return false;
+    //// фібоначчі
+    //public static class IntExtensions
+    //{
+    //    public static bool IsFibonacciNumber(this int number)
+    //    {
+    //        if (number < 0)
+    //            return false;
 
-            int a = 0;
-            int b = 1;
+    //        int a = 0;
+    //        int b = 1;
 
-            while (b < number)
-            {
-                int temp = a;
-                a = b;
-                b = temp + b;
-            }
+    //        while (b < number)
+    //        {
+    //            int temp = a;
+    //            a = b;
+    //            b = temp + b;
+    //        }
 
-            return b == number;
-        }
-    }
-    //кількість слів у рдку||кількість символів в останньому слові
-    public static class StringExtensions
-    {
-        public static int WordCount(this string str)
-        {
-            return str.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries).Length;// підглянув цей код в неті сподобалося що не рахує пусті під рядки коли два пробіли підряд наприклад
-        }
-        public static int LengthOfLastWord(this string str)
-        {
-            if (string.IsNullOrWhiteSpace(str))
-                return 0;
+    //        return b == number;
+    //    }
+    //}
+    ////кількість слів у рдку||кількість символів в останньому слові
+    //public static class StringExtensions
+    //{
+    //    public static int WordCount(this string str)
+    //    {
+    //        return str.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries).Length;// підглянув цей код в неті сподобалося що не рахує пусті під рядки коли два пробіли підряд наприклад
+    //    }
+    //    public static int LengthOfLastWord(this string str)
+    //    {
+    //        if (string.IsNullOrWhiteSpace(str))
+    //            return 0;
 
-            string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            if (words.Length > 0)
-            {
-                string lastWord = words[words.Length - 1];
-                return lastWord.Length;
-            }
+    //        string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    //        if (words.Length > 0)
+    //        {
+    //            string lastWord = words[words.Length - 1];
+    //            return lastWord.Length;
+    //        }
 
-            return 0;
-        }
-        //перевіряє правильність розстановки дужок за допомогою стеку
-        public static bool BracketPlacement(this string input)
-        {
-            Stack<char> stack = new Stack<char>();
+    //        return 0;
+    //    }
+    //    //перевіряє правильність розстановки дужок за допомогою стеку
+    //    public static bool BracketPlacement(this string input)
+    //    {
+    //        Stack<char> stack = new Stack<char>();
 
-            foreach (char c in input)
-            {
-                if (IsOpeningParenthesis(c))
-                {
-                    stack.Push(c);
-                }
-                else if (IsClosingParenthesis(c))
-                {
-                    if (stack.Count == 0 || !AreParenthesesMatching(stack.Peek(), c))
-                    {
-                        return false;
-                    }
+    //        foreach (char c in input)
+    //        {
+    //            if (IsOpeningParenthesis(c))
+    //            {
+    //                stack.Push(c);
+    //            }
+    //            else if (IsClosingParenthesis(c))
+    //            {
+    //                if (stack.Count == 0 || !AreParenthesesMatching(stack.Peek(), c))
+    //                {
+    //                    return false;
+    //                }
 
-                    stack.Pop();
-                }
-            }
+    //                stack.Pop();
+    //            }
+    //        }
 
-            return stack.Count == 0;
-        }
-        private static bool IsOpeningParenthesis(char c)
-        {
-            return c == '(' || c == '[' || c == '{';
-        }
+    //        return stack.Count == 0;
+    //    }
+    //    private static bool IsOpeningParenthesis(char c)
+    //    {
+    //        return c == '(' || c == '[' || c == '{';
+    //    }
 
-        private static bool IsClosingParenthesis(char c)
-        {
-            return c == ')' || c == ']' || c == '}';
-        }
+    //    private static bool IsClosingParenthesis(char c)
+    //    {
+    //        return c == ')' || c == ']' || c == '}';
+    //    }
 
-        private static bool AreParenthesesMatching(char opening, char closing)
-        {
-            return (opening == '(' && closing == ')') || (opening == '[' && closing == ']') || (opening == '{' && closing == '}');
-        }
-    }
-    //метод для фільтрації масиву за умовою
-    public static class IntArrayExtensions
-    {
-        public static int[] Filter(this int[] array, Predicate<int> predicate)
-        {
-            int[] filteredArray = Array.FindAll(array, predicate);
-            return filteredArray;
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //--1--//
-            Console.Write("Введіть число: ");
-            int Number = int.Parse(Console.ReadLine());
-            bool isFibonacci = Number.IsFibonacciNumber();
+    //    private static bool AreParenthesesMatching(char opening, char closing)
+    //    {
+    //        return (opening == '(' && closing == ')') || (opening == '[' && closing == ']') || (opening == '{' && closing == '}');
+    //    }
+    //}
+    ////метод для фільтрації масиву за умовою
+    //public static class IntArrayExtensions
+    //{
+    //    public static int[] Filter(this int[] array, Predicate<int> predicate)
+    //    {
+    //        int[] filteredArray = Array.FindAll(array, predicate);
+    //        return filteredArray;
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //--1--//
+    //        Console.Write("Введіть число: ");
+    //        int Number = int.Parse(Console.ReadLine());
+    //        bool isFibonacci = Number.IsFibonacciNumber();
 
-            Console.WriteLine($"{Number} is {(isFibonacci ? "a Fibonacci number" : "not a Fibonacci number")}");
-            //--2--//
-            string text = "Hello, world!";
+    //        Console.WriteLine($"{Number} is {(isFibonacci ? "a Fibonacci number" : "not a Fibonacci number")}");
+    //        //--2--//
+    //        string text = "Hello, world!";
 
-            int wordCount = text.WordCount();
-            Console.WriteLine($"Word count: {wordCount}");
-            //--3--//
-            int lengthOfLastWord = text.LengthOfLastWord();//рахує разом з '!', ящо добавити до розділювача то не рахуватиме
+    //        int wordCount = text.WordCount();
+    //        Console.WriteLine($"Word count: {wordCount}");
+    //        //--3--//
+    //        int lengthOfLastWord = text.LengthOfLastWord();//рахує разом з '!', ящо добавити до розділювача то не рахуватиме
 
-            Console.WriteLine($"Text: {text}");
-            Console.WriteLine($"Length of Last Word: {lengthOfLastWord}");
-            //--4--//
-            string text1 = "({bla bla}[bla])";
-            Console.WriteLine($"{text1} is {(text1.BracketPlacement() ? "valid" : "invalid")}");
-            string text2 = "((bla bla}[bla])";
-            Console.WriteLine($"{text2} is {(text2.BracketPlacement() ? "valid" : "invalid")}");
-            //--5--//
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int[] evenNumbers = numbers.Filter(x => x % 2 == 0);
-            Console.WriteLine("Парні числа:");
-            foreach (int number in evenNumbers)
-            {
-                Console.WriteLine(number);
-            }
-            int[] highNumbers = numbers.Filter(x => x > 5);
-            Console.WriteLine("Великі числа:");
-            foreach (int number in highNumbers)
-            {
-                Console.WriteLine(number);
-            }
-        }
-    }
+    //        Console.WriteLine($"Text: {text}");
+    //        Console.WriteLine($"Length of Last Word: {lengthOfLastWord}");
+    //        //--4--//
+    //        string text1 = "({bla bla}[bla])";
+    //        Console.WriteLine($"{text1} is {(text1.BracketPlacement() ? "valid" : "invalid")}");
+    //        string text2 = "((bla bla}[bla])";
+    //        Console.WriteLine($"{text2} is {(text2.BracketPlacement() ? "valid" : "invalid")}");
+    //        //--5--//
+    //        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    //        int[] evenNumbers = numbers.Filter(x => x % 2 == 0);
+    //        Console.WriteLine("Парні числа:");
+    //        foreach (int number in evenNumbers)
+    //        {
+    //            Console.WriteLine(number);
+    //        }
+    //        int[] highNumbers = numbers.Filter(x => x > 5);
+    //        Console.WriteLine("Великі числа:");
+    //        foreach (int number in highNumbers)
+    //        {
+    //            Console.WriteLine(number);
+    //        }
+    //    }
+    //}
 }
 
 
---
+++ C#DZ/26_05_2023/26_05_2023.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_DZ._26_05_2023
+{
+    //class Backpack
+    //{
+    //    public string Color { get; set; }
+    //    public string Brand { get; set; }
+    //    public string Fabric { get; set; }
+    //    public double Weight { get; set; }
+    //    public double Volume { get; set; }
+    //    public List<Item> Contents { get; private set; }
+
+    //    public event Action<Item> ItemAdded;
+
+    //    public Backpack(string color, string brand, string fabric, double weight, double volume)
+    //    {
+    //        Color = color;
+    //        Brand = brand;
+    //        Fabric = fabric;
+    //        Weight = weight;
+    //        Volume = volume;
+    //        Contents = new List<Item>();
+    //    }
+
+    //    public void AddItem(Item item)
+    //    {
+    //        if (GetTotalOccupiedVolume() + item.Volume > Volume)
+    //        {
+    //            throw new Exception("Exceeded backpack volume.");
+    //        }
+
+    //        Contents.Add(item);
+    //        ItemAdded?.Invoke(item);
+    //    }
+
+    //    public double GetTotalOccupiedVolume()
+    //    {
+    //        double totalVolume = 0;
+    //        foreach (var item in Contents)
+    //        {
+    //            totalVolume += item.Volume;
+    //        }
+    //        return totalVolume;
+    //    }
+    //}
+
+    //class Item
+    //{
+    //    public string Name { get; set; }
+    //    public double Volume { get; set; }
+
+    //    public Item(string name, double volume)
+    //    {
+    //        Name = name;
+    //        Volume = volume;
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //--1--//
+    //        string color = "yellow";
+
+    //        Func<string, string> getRGBValue = delegate (string c)
+    //        {
+    //            switch (c.ToLower())
+    //            {
+    //                case "red":
+    //                    return "255, 0, 0";
+    //                case "orange":
+    //                    return "255, 165, 0";
+    //                case "yellow":
+    //                    return "255, 255, 0";
+    //                case "green":
+    //                    return "0, 128, 0";
+    //                case "blue":
+    //                    return "0, 0, 255";
+    //                case "indigo":
+    //                    return "75, 0, 130";
+    //                case "violet":
+    //                    return "238, 130, 238";
+    //                default:
+    //                    return "Invalid color";
+    //            }
+    //        };
+
+    //        string rgbValue = getRGBValue(color);
+    //        Console.WriteLine($"RGB value for {color}: {rgbValue}");
+
+    //        //--2--//
+    //        Backpack backpack = new Backpack("Blue", "ABC Company", "Nylon", 1.5, 10);
+    //        backpack.ItemAdded += item => Console.WriteLine($"Added item to backpack: {item.Name}");//підписуємо лямбда функцію виводу інформації на екран на подію яка викликається в методі добавити придмет
+
+    //        try
+    //        {
+    //            Item item1 = new Item("Book", 2);
+    //            backpack.AddItem(item1);
+
+    //            Item item2 = new Item("Water Bottle", 0.5);
+    //            backpack.AddItem(item2);
+
+    //            Item item3 = new Item("Laptop acer", 4);
+    //            backpack.AddItem(item3);
+
+    //            Item item4 = new Item("Laptop hp", 4);
+    //            backpack.AddItem(item3);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine(ex.Message);
+    //        }
+
+    //        Console.WriteLine($"Total occupied volume: {backpack.GetTotalOccupiedVolume()}");
+    //        //--3--//
+    //        int[] numbers = { 1, 21, 35, 44, 75, 63, 70, 48, 93, 11 };
+    //        int count = numbers.Count(x => x % 7 == 0);//лямбда вираз виступає предикатом для функції Count
+    //        Console.WriteLine($"Count of numbers divisible by seven: {count}");
+    //        //--4--//
+    //        int[] numbers1 = { -1, 21, -35, 44, 75, -63, 70, -48, 93, -11 };
+    //        int count1 = numbers1.Count(x => x < 0);//лямбда вираз виступає предикатом для функції Count
+    //        Console.WriteLine($"Count of numbers less then zero: {count1}");
+    //        //--5--//
+    //        int[] numbers2 = { -1, 21, -35, 44, 75, -35, 70, -48, 93, -35 };
+
+    //        HashSet<int> uniqueNegativeNumbers = new HashSet<int>(numbers2.Where(number => number < 0));
+
+    //        string result = string.Join(", ", uniqueNegativeNumbers);// хотів просто щоб було в рядок
+    //        Console.WriteLine($"Unique numbers less then zero: {result}");
+    //        //спочатку зробив так але виявилося що можна набагато простіше
+    //        //Array.ForEach(numbers2, number =>
+    //        //{
+    //        //    if (number < 0 && uniqueNegativeNumbers.Add(number))
+    //        //    {
+    //        //        Console.WriteLine(number);
+    //        //    }
+    //        //});
+    //        //--6--//
+    //        string text = "This is a test text";
+    //        string targetWord = "test";
+
+    //        Func<string, string, bool> containsWord = (inputText, word) => inputText.Contains(word);
+
+    //        bool containsTargetWord = containsWord(text, targetWord);
+    //        Console.WriteLine($"Text {(containsTargetWord ? "contains" : "does not contain")} the word '{targetWord}'");
+
+    //    }
+    //}
+}
